41c33570df4bf42492b5e45e165b7979e24d238d
Iñaki Pujol <ipujol10@gmail.com>
2023-07-22 11:11:37 +0100

    UPDATE: first steps to generate files

--- src/package_files_generator/generator.py
+++ src/package_files_generator/generator.py
@@ -1,3 +1,11 @@
+import os
+def generate(package_name: str) -> None:
+    path = f"tests/cases/{package_name}"
+    os.mkdir(f"{path}/src")
+    os.mkdir(f"{path}/src/{package_name}")
+    os.mkdir(f"{path}/tests")
+
+
 def main() -> None:
     raise NotImplementedError
 
--- tests/test_generation.py
+++ tests/test_generation.py
@@ -1,5 +1,7 @@
 import unittest
 import os
+from package_files_generator.generator import generate
+import shutil
 
 
 class TestPackaging(unittest.TestCase):
@@ -9,8 +11,20 @@ class TestPackaging(unittest.TestCase):
         if (not os.path.isdir(self.cases)):
             os.makedirs(self.cases)
 
+    def tearDown(self) -> None:
+        shutil.rmtree(f"{self.cases}/test_empty")
+
     def test_no_directory(self) -> None:
-        dir = f"{self.cases}/empty"
+        package_name = "test_empty"
+        dir = f"{self.cases}/{package_name}"
         self.assertFalse(os.path.isdir(dir))
         os.makedirs(dir)
         self.assertTrue(os.path.isdir(dir))
+        generate(package_name)
+        self.assertTrue(os.path.isdir(f"{dir}/src/{package_name}"))
+        self.assertTrue(os.path.isdir(f"{dir}/tests"))
+        self.assertTrue(os.path.isfile(f"{dir}/LICENSE"))
+        self.assertTrue(os.path.isfile(f"{dir}/pyproject.toml"))
+        self.assertTrue(os.path.isfile(f"{dir}/README.md"))
+        self.assertTrue(os.path.isfile(f"{dir}/src/{package_name}/\
+                __init__.py"))
